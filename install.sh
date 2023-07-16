a07d25b64c090ffec80207abeba3a9a4e396eb3e
doggystylez <98429202+doggystylez@users.noreply.github.com>
2023-07-16 11:47:26 -0400

    see last commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+[ "$(basename $(pwd))" == "cosmos-env" ] || { echo "must be run from cosmos-env directory" ; exit 1 ; }
+[ -z $(which git) ] && { echo "git required" ; exit 1 ; }
+[ -z $(which lxd) ] && { echo "lxd required" ; exit 1 ; }
+[ -z $(which jq) ] && { echo "jq required" ; exit 1 ; }
+
+rm -rf ~/.cosmos-env && mkdir ~/.cosmos-env
+for i in $(ls); do
+  [ "$i" != "install.sh" ] && [ "$i" != "README.md" ] && cp -r $i ~/.cosmos-env/
+done
+chmod +x ~/.cosmos-env/{cosmwasm/upload,cosmos-go/init_node}
+[ -z "$(grep ". ~/.cosmos-env/load" ~/.bashrc)" ] && echo -e "\n. ~/.cosmos-env/load" >> ~/.bashrc
+
+source ~/.bashrc
